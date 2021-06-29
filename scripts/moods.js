b9414aedb78d3f01dd7f8f1b5b4315f7fcf0dbce
tarun2506 <tarunsharma8920@gmail.com>
2021-06-29 21:06:50 +0530

    cards finalized on mood-lists page

--- scripts/moods.js
+++ scripts/moods.js
@@ -1,5 +1,6 @@
 // Getting dom elements:
 const cardsContainer = document.getElementById("cards");
+const floatBtn = document.getElementById("floating-btn");
 let userId;
 const appMoods = {
   0: "Happy",
@@ -33,51 +34,57 @@ auth.onAuthStateChanged((u) => {
 function renderCard(Mood) {
   let cardDiv = document.createElement("div");
   let cardContentDiv = document.createElement("div");
-  let date = document.createElement("h6");
-  let time = document.createElement("h5");
-  let mood = document.createElement("h5");
+  let topDiv = document.createElement("div");
+  let midDiv = document.createElement("div");
+  let bottomDiv = document.createElement("div");
+  let buttonsDiv = document.createElement("div");
+  let date = document.createElement("p");
+  let time = document.createElement("p");
+  let mood = document.createElement("p");
   let note = document.createElement("p");
-  let editButton = document.createElement("button");
-  let deleteButton = document.createElement("button");
   let editlink = document.createElement("a");
   let deletelink = document.createElement("a");
 
   cardDiv.setAttribute("id", "card");
   cardDiv.setAttribute("data-id", Mood.id);
   cardContentDiv.setAttribute("id", "card-content");
+  topDiv.classList.add("top");
+  midDiv.classList.add("mid");
+  bottomDiv.classList.add("bottom");
+  buttonsDiv.classList.add("buttons");
   date.setAttribute("id", "date");
   time.setAttribute("id", "time");
   mood.setAttribute("id", "mood");
   note.setAttribute("id", "note");
-  editButton.setAttribute("id", "edit-btn");
-  editButton.classList.add("button");
-  deleteButton.classList.add("button");
-  deleteButton.setAttribute("id", "delete-btn");
   editlink.setAttribute("href", `update-mood.html?id=${Mood.id}`);
 
   date.innerText = Mood.data().date;
   time.innerText = Mood.data().time;
   mood.innerText = appMoods[Mood.data().yourMood];
   note.innerText = Mood.data().Note;
-  editlink.innerText = "Edit";
-  deletelink.innerText = "Delete";
+  // editlink.innerText = "Edit";
+  editlink.innerHTML = `<i class="material-icons small">create</i>`;
+  deletelink.innerHTML = `<i class="material-icons small">clear</i>`;
+  // deletelink.innerText = "Delete";
 
-  editButton.appendChild(editlink);
-  deleteButton.appendChild(deletelink);
-  cardContentDiv.appendChild(date);
-  cardContentDiv.appendChild(time);
-  cardContentDiv.appendChild(mood);
-  cardContentDiv.appendChild(note);
-  cardContentDiv.appendChild(editButton);
-  cardContentDiv.appendChild(deleteButton);
+  buttonsDiv.appendChild(editlink);
+  buttonsDiv.appendChild(deletelink);
+  bottomDiv.appendChild(time);
+  bottomDiv.appendChild(buttonsDiv);
+  midDiv.appendChild(note);
+  topDiv.appendChild(date);
+  topDiv.appendChild(mood);
+  cardContentDiv.appendChild(topDiv);
+  cardContentDiv.appendChild(midDiv);
+  cardContentDiv.appendChild(bottomDiv);
   cardDiv.appendChild(cardContentDiv);
   cardsContainer.appendChild(cardDiv);
 
-  editButton.addEventListener("click", () => {
+  editlink.addEventListener("click", () => {
     window.location.href = `update-mood.html?id=${Mood.id}`;
   });
 
-  deleteButton.addEventListener("click", async (e) => {
+  deletelink.addEventListener("click", async (e) => {
     let id = cardDiv.getAttribute("data-id");
     await db
       .collection("users")
@@ -88,3 +95,7 @@ function renderCard(Mood) {
     window.location.reload();
   });
 }
+
+floatBtn.addEventListener("click", (e) => {
+  window.location.href = "add-mood.html";
+});
