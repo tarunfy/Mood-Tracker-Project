17d166481ae64a4b1f780dea2547ae2347552c77
tarun2506 <tarunsharma8920@gmail.com>
2021-07-15 01:53:35 +0530

    average mood card functionality added

--- scripts/insights.js
+++ scripts/insights.js
@@ -8,6 +8,18 @@ const currentDate = moment(date).format("YYYY-MM-DD");
 const a = moment().subtract(7, "days").calendar();
 const previousDate = moment(a).format("YYYY-MM-DD");
 
+let count = 0;
+let sum = 0;
+let avg;
+let moods = {
+  0: "Happy",
+  1: "Sad",
+  2: "Angry",
+  3: "Cheerful",
+  4: "Nervous",
+  5: "Peaceful",
+  6: "Optimistic",
+};
 // Checking if user is present:
 auth.onAuthStateChanged((u) => {
   if (u == null) {
@@ -18,23 +30,38 @@ auth.onAuthStateChanged((u) => {
   db.collection("users")
     .doc(u.uid)
     .collection("moods")
-    .orderBy("date")
-    .orderBy("time")
     .get()
     .then((snapshot) => {
-      snapshot.docs.forEach((mood) => {
-        updateDetails(mood.data());
-      });
+      updateDetails();
     })
     .catch((error) => {
       console.log(error.message);
     });
 });
 
-function updateDetails(details) {
-  toDateInput.value = currentDate;
-  fromDateInput.value = previousDate;
-  const moodHeading = document.createElement("h3");
-  moodHeading.innerText = "Sad";
-  mood.appendChild(moodHeading);
+function updateDetails() {
+  db.collection("users")
+    .doc(userId)
+    .collection("moods")
+    .where("date", "<=", currentDate)
+    .where("date", ">=", previousDate)
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        sum += Number(doc.data().yourMood);
+        count += 1;
+      });
+      avg = Math.round(sum / count);
+      console.log(avg);
+      console.log(sum);
+      console.log(count);
+      toDateInput.value = currentDate;
+      fromDateInput.value = previousDate;
+      const moodHeading = document.createElement("h3");
+      moodHeading.innerText = moods[avg];
+      mood.appendChild(moodHeading);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
